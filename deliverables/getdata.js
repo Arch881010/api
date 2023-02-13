a14150b48db3e2b14c140139c48da09a9967864b
Arch <andrew.archer@hgsd1.com>
2023-02-13 09:28:58 -0600

    Feat: Remove need for error msg. (Forces .json to have [])

--- deliverables/getdata.js
+++ deliverables/getdata.js
@@ -1,28 +1,12 @@
 const fs = require('fs')
-const datarecieved = []
+var datarecieved = "";
 async function data() {
 const apikey = fs.readFileSync("deliverables/userapikey.txt","UTF-8");
 url = `http://127.0.0.1:5000/data|${apikey}`
 const dataftch = await fetch(url)
-try{
-dataval = await dataftch.json()
-} catch (err) {
-    dataval = [
-        {
-            "id":404,
-            "error":true,
-            "name":"error-fetch",
-            "description":"The URL did not respond with a .json document. This could because the apikey is incorrect OR the url was moved.",
-            "task":"Main"
-        }
-    ]
-}
-datarecieved.push(dataval[0])
-if(dataval[0].error == true) {
-console.log(dataval)
-} else {
+dataval = await dataftch.text()
+datarecieved = dataval;
 console.log(datarecieved)
-}
 }
 data()
 //Call with "npm run main"
